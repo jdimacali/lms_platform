09627128f15e58c3ae623dc610e391a7193e2678
James Dimacali <frostbitezebra421@gmail.com>
2023-10-04 19:16:49 -0700

    initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Hello World</h1>
+      <Button variant={"outline"}>Click Me</Button>
+    </main>
+  );
+}
