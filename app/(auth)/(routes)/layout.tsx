82562274c84307944382a7fc2c1d0b8485c07675
James Dimacali <frostbitezebra421@gmail.com>
2023-10-04 20:07:10 -0700

    Clerk Authentication initialized

---
+++ app/(auth)/(routes)/layout.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full flex justify-center items-center">{children}</div>
+  );
+};
+export default layout;
