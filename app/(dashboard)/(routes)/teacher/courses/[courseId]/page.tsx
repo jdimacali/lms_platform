caee56fc84e5a0dec22add9ff4e36073886f4c1a
James Dimacali <frostbitezebra421@gmail.com>
2023-10-06 13:31:25 -0700

    created api for updating course title and form

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,14 +1,69 @@
-"use client";
+import { auth } from "@clerk/nextjs";
+import { LayoutDashboard } from "lucide-react";
+import { redirect } from "next/navigation";
 
-import { useParams } from "next/navigation";
+import TitleForm from "./_components/TitleForm";
+import { IconBadge } from "@/components/IconBadge";
+import { db } from "@/lib/db";
 
-const CoursePage = ({
+const CoursePage = async ({
   params,
 }: {
   params: {
     courseId: string;
   };
 }) => {
-  return <div> Course Id : {params.courseId} </div>;
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/sign-in");
+  }
+
+  const course = await db.course.findFirst({
+    where: {
+      id: params.courseId,
+      userId,
+    },
+  });
+
+  if (!course) {
+    return redirect("/teacher/create");
+  }
+
+  const requiredFields = [
+    course.title,
+    course.description,
+    course.imageUrl,
+    course.price,
+    course.categoryId,
+  ];
+
+  const totalFields = requiredFields.length;
+  // checks each item in the array if it is true or false and how many are truthy
+  const completedFields = requiredFields.filter(Boolean).length;
+
+  const completionText = `(${completedFields}/${totalFields})`;
+
+  return (
+    <div className="p-6">
+      <div className="flex items-center justify-between">
+        <div className="flex flex-col gap-y-2">
+          <h1 className="text-3xl font-medium"> Course setup</h1>
+          <span className="text-sm text-slate-700">
+            Complete all fields {completionText}
+          </span>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16">
+        <div>
+          <div className="flex items-center gap-x-2">
+            <IconBadge icon={LayoutDashboard} />
+            <h2 className="text-xl"> Customize your course </h2>
+          </div>
+          <TitleForm initialData={course} courseId={course.id} />
+        </div>
+      </div>
+    </div>
+  );
 };
 export default CoursePage;
--- app/(dashboard)/_components/Logo.tsx
+++ app/(dashboard)/_components/Logo.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 const Logo = () => {
   return (
-    <Image height={130} width={130} alt="logo" src="logo.svg" priority={true} />
+    <Image height={130} width={130} alt="logo" src="/logo.svg" priority={true} />
   );
 };
 export default Logo;
