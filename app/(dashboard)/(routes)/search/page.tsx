3cf11a6f181949f2943129c9a15e8bd3ed986987
James Dimacali <frostbitezebra421@gmail.com>
2023-10-11 16:00:13 -0700

    categories buttons

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,4 +1,17 @@
-const SearchPage = () => {
-  return <div>SearchPage</div>;
+import { db } from "@/lib/db";
+import Categories from "./_components/Categories";
+
+const SearchPage = async () => {
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: "asc",
+    },
+  });
+
+  return (
+    <div className="p-6 ">
+      <Categories items={categories} />
+    </div>
+  );
 };
 export default SearchPage;
