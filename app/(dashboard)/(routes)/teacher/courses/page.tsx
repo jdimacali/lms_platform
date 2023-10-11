2e000ab6c1e0a4f763645ab0adcb8d3f8c321f86
James Dimacali <frostbitezebra421@gmail.com>
2023-10-10 18:52:33 -0700

    courses data table and columns

--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,13 +1,29 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { redirect } from "next/navigation";
+import { columns } from "./_components/Columns";
+import { DataTable } from "./_components/DataTable";
+import { auth } from "@clerk/nextjs";
+import { db } from "@/lib/db";
 
-const page = () => {
+const CoursesPage = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const courses = await db.course.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
   return (
     <div className="p-6">
-      <Link href="/teacher/create">
-        <Button> New Course </Button>
-      </Link>
+      <DataTable columns={columns} data={courses} />
     </div>
   );
 };
-export default page;
+export default CoursesPage;
