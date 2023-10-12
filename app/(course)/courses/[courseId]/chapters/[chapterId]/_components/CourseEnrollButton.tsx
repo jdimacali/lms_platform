3c012e8c5b257be062ab0d815156be9a7db98b70
James Dimacali <frostbitezebra421@gmail.com>
2023-10-12 14:50:27 -0700

    course chapter page details

---
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/CourseEnrollButton.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { formatPrice } from "@/lib/format";
+
+interface CourseEnrollButtonProps {
+  price: number;
+  courseId: string;
+}
+
+const CourseEnrollButton = ({ price, courseId }: CourseEnrollButtonProps) => {
+  return (
+    <Button className="w-full md:w-auto" size="sm">
+      Enroll for {formatPrice(price)}
+    </Button>
+  );
+};
+export default CourseEnrollButton;
