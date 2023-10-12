3ddc22b27969735f83f50c3a20e70d5aed028464
James Dimacali <frostbitezebra421@gmail.com>
2023-10-12 02:54:22 -0700

    course sidebar mobile sidebaritem and nav

---
+++ app/(course)/courses/[courseId]/page.tsx
@@ -0,0 +1,30 @@
+import { db } from "@/lib/db";
+import { redirect } from "next/navigation";
+
+const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
+  const course = await db.course.findUnique({
+    where: {
+      id: params.courseId,
+    },
+    include: {
+      chapters: {
+        where: {
+          isPublished: true,
+        },
+        orderBy: {
+          position: "asc",
+        },
+      },
+    },
+  });
+
+  if (!course) {
+    return redirect("/");
+  }
+
+  return <div>
+    CourseIdPage
+  </div>
+};
+
+export default CourseIdPage;
