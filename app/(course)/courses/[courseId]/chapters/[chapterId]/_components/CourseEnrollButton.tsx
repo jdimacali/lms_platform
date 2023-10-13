2621daa81d725a91489b7766352079e1f851b14e
James Dimacali <frostbitezebra421@gmail.com>
2023-10-12 18:32:27 -0700

    stripe api functions and buy

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/CourseEnrollButton.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/CourseEnrollButton.tsx
@@ -2,6 +2,9 @@
 
 import { Button } from "@/components/ui/button";
 import { formatPrice } from "@/lib/format";
+import axios from "axios";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
 interface CourseEnrollButtonProps {
   price: number;
@@ -9,8 +12,27 @@ interface CourseEnrollButtonProps {
 }
 
 const CourseEnrollButton = ({ price, courseId }: CourseEnrollButtonProps) => {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onClick = async () => {
+    try {
+      setIsLoading(true);
+      const response = await axios.post(`/api/courses/${courseId}/checkout`);
+      window.location.assign(response.data.url);
+    } catch (error) {
+      toast.error("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <Button className="w-full md:w-auto" size="sm">
+    <Button
+      className="w-full md:w-auto"
+      size="sm"
+      onClick={onClick}
+      disabled={isLoading}
+    >
       Enroll for {formatPrice(price)}
     </Button>
   );
